dbe0002cd4c6cad23320ef646f07d9f51aa18b39
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-02-15 13:52:51 -0700

    Add .env for testflight
    Added ci prebuild for xcode cloud to create .env file

---
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Stage: PRE-Xcode Build is activated .... "
+
+echo "XMTP_ENV=${XMTP_ENV}" > "../../.env"
+echo "THRID_WEB_CLIENT_ID=${THRID_WEB_CLIENT_ID}" > "../../.env"
+echo "AWS_S3_REGION=${AWS_S3_REGION}" > "../../.env"
+echo "AWS_S3_BUCKET=${AWS_S3_BUCKET}" > "../../.env"
+echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" > "../../.env"
+echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" > "../../.env"
+
+exit 0
