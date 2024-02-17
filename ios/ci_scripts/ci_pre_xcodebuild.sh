6d7f43c11cfa0f5b396b3bc8908b62a7e0fbe190
Ethan Mateja <ethan.mateja@icloud.com>
2024-02-16 20:55:47 -0800

    Updated CI Prebuild

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-echo "Stage: PRE-Xcode Build is activated .... "
+echo "####===== Stage: PRE-Xcode Build is activated .... "
 
-echo "XMTP_ENV=${XMTP_ENV}" > "../../.env"
-echo "THRID_WEB_CLIENT_ID=${THRID_WEB_CLIENT_ID}" > "../../.env"
-echo "AWS_S3_REGION=${AWS_S3_REGION}" > "../../.env"
-echo "AWS_S3_BUCKET=${AWS_S3_BUCKET}" > "../../.env"
-echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" > "../../.env"
-echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" > "../../.env"
+cat > ../../.env <<EOF
+XMTP_ENV=${XMTP_ENV}
+THRID_WEB_CLIENT_ID=${THRID_WEB_CLIENT_ID}
+AWS_S3_REGION=${AWS_S3_REGION}
+AWS_S3_BUCKET=${AWS_S3_BUCKET}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+EOF
 
-echo "###===== Run Yarn Install"
-yarn install
-
-echo "###===== Temporary Debug Env File Output"
+echo "####===== Temporary ENV Debug"
 cat ../../.env
+
+echo "####===== Run Yarn Install"
+yarn install
 
 exit 0
